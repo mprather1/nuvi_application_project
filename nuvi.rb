38b9d69aaa69cf772ff9f73ef7444ba89d18cd22
Michael Prather <mprather1@gmail.com>
2016-08-30 19:53:06 -0500

    cleaned up

--- nuvi.rb
+++ nuvi.rb
@@ -1,7 +1,6 @@
 require "zip"
 require 'nokogiri'
 require 'open-uri'
-require 'net/http'
 require 'redis'
 redis = Redis.new
 
@@ -10,20 +9,15 @@ redis = Redis.new
 @target = "/" + @uri.split("//")[1]
 @dir = Dir["temp/#{@target}*.zip"]
 
-# def unzip(arg)
-#   `unzip "#{arg}" -d data`
-# end
-
 page = Nokogiri::HTML(open(@uri))
 
 page.xpath('//a/@href').each do |links|
   @urls << links.content
 end
-# 
-# @urls.each do |url|
-#   `wget -r -nc -np -l 1 -A zip -P "./temp" "#{@uri}#{url}"`
-# end
 
+@urls.each do |url|
+  `wget -r -nc -np -l 1 -A zip -P "./temp" "#{@uri}#{url}"`
+end
 
 @dir.each do |d|
   Zip::File.open(d) do |zip_file|
@@ -32,53 +26,3 @@ end
     end
   end
 end
-
-# @dir.each do |f|
-#   unzip(f)
-# end
-
-# redis.name
-redis.set "foo", "bar"
-puts redis.get("foo")
-
-# @urls.each do |url|
-#   open("#{url}", 'wb') do |fo|
-#   fo.print open("http://feed.omgili.com/5Rh5AMTrc4Pv/mainstream/posts/#{url}").read
-# end
-# end
-
-# @urls.each do |url|
-#   open(url, 'wb') do |f|
-#     f.print open("http://feed.omgili.com/5Rh5AMTrc4Pv/mainstream/posts/#{url}").read
-#   end
-# end
-
-
-# open("1472263084392.zip", 'wb') do |fo|
-#   fo.print open("http://feed.omgili.com/5Rh5AMTrc4Pv/mainstream/posts/1472263084392.zip").read
-# end
-# @urls.each do |url|
-  # `wget "http://feed.omgili.com/5Rh5AMTrc4Pv/mainstream/posts/#{url}"`
-#   File.open("#{url}", 'wb') do |fo|
-#     puts "#{url}"# fo.write open("http://feed.omgili.com/5Rh5AMTrc4Pv/mainstream/posts/").read
-#   end
-#     # open("/files", "wb") do |file|
-#     #   file.write(resp.body)
-#     # end
-#   # end
-# end
-# puts @urls
-# Net::HTTP.start('http://feed.omgili.com/5Rh5AMTrc4Pv/mainstream/posts/') { |http|
-#   @urls.length.times do |i|
-
-# }
-# end
-
-# @urls.length.times do |i|
-  # Net::HTTP.start(i) do |http|
-  #   resp = http.get("/.zip")
-  #   open("/files", "wb") do |file|
-  #     file.write(resp.body)
-  #   end
-  # end
-# end
