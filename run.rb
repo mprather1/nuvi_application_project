035b41912a3d375c9449d2171534d7a49a0ff18a
Michael Prather <mprather1@gmail.com>
2016-09-01 03:47:55 +0000

    all done press any key to exit

--- run.rb
+++ run.rb
@@ -1,7 +1,13 @@
 require_relative 'app'
+require 'io/console'
 
 app = Scraper.new
 app.target = "http://feed.omgili.com/5Rh5AMTrc4Pv/mainstream/posts/"
 app.download_files
 app.extract_zip_files
 app.to_redis
+
+puts "All done..."
+print "Press any key to exit..."
+STDIN.getch
+print "\n"
