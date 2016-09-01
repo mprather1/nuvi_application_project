9087bb1340d33fdbf93f7d13d9a339d17815d48d
Michael Prather <mprather1@gmail.com>
2016-09-01 23:30:23 +0000

    fixed run.rb

--- run.rb
+++ run.rb
@@ -3,9 +3,9 @@ require 'io/console'
 
 app = Scraper.new
 app.target = "http://feed.omgili.com/5Rh5AMTrc4Pv/mainstream/posts/"
-# app.download_files
+app.download_files
 app.extract_zip_files
-# app.to_redis
+app.to_redis
 
 puts "All done..."
 puts "Press any key to exit..."
