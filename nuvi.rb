cd2c4f50f8eab9ca7fb2585d1d53b8e8eb535ea3
Michael Prather <mprather1@gmail.com>
2016-09-01 00:02:37 +0000

    added class and functions

--- nuvi.rb
+++ nuvi.rb
@@ -3,34 +3,60 @@ require 'nokogiri'
 require 'open-uri'
 require 'redis'
 
-r = Redis.new
-r.del 'NEWS_XML'
-
-@urls = []
-@target = "http://feed.omgili.com/5Rh5AMTrc4Pv/mainstream/posts/"
-@dir = Dir["temp/" + @target.split("//")[1] + "*.zip"]
-
-page = Nokogiri::HTML(open(@target))
-
-page.xpath('//a/@href').each do |links|
-  @urls << links.content
-end
-
-@urls.each do |url|
-  `wget -r -nc -np -l 1 -A zip -P "./temp" "#{@target}#{url}"`
-end
-
-@dir.each do |d|
-  Zip::File.open(d) do |zip_file|
-    zip_file.each do |f|
-      f.extract("data/#{f}") { true }
-    end
+class Application
+  
+  attr_accessor :urls
+  attr_reader :dir, :target
+  
+  def initialize
+    
+    @r = Redis.new
+    @r.del 'NEWS_XML'
+    
+    @urls = []
+    @target = "http://feed.omgili.com/5Rh5AMTrc4Pv/mainstream/posts/"
+    @dir = Dir["temp/" + @target.split("//")[1] + "*.zip"]
+  
   end
+  
+  def download_files
+    
+    page = Nokogiri::HTML(open(@target))
+
+    page.xpath('//a/@href').each do |links|
+      @urls << links.content
+    end
+    
+    @urls.each do |url|
+      `wget -r -nc -np -l 1 -A zip -P "./temp" "#{@target}#{url}"`
+    end
+    
+  end
+  
+  def extract_zip_files
+    
+    @dir.each do |d|
+      Zip::File.open(d) do |zip_file|
+      zip_file.each do |f|
+        f.extract("data/#{f}") { true }
+      end
+    end
+    
+  end
+  
+  def import_to_redis
+    
+    Dir["data/*.xml"].each do |f|
+      xmldoc = Nokogiri::XML(File.open(f))
+        @r.sadd "NEWS_XML", "#{xmldoc}"
+      end
+    end
+    
+  end
+  
 end
 
-Dir["data/*.xml"].each do |f|
-  xmldoc = Nokogiri::XML(File.open(f))
-  r.sadd "NEWS_XML", "#{xmldoc}"
-end
-
-# puts r.smembers('NEWS_XML')
+applicationProject = Application.new
+applicationProject.download_files
+applicationProject.extract_zip_files
+applicationProject.import_to_redis
